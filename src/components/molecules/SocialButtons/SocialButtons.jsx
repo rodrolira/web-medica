8b7f7af4e712df9672c0fdcb4919bd3b1abb80eb
rodrolira <rlirapizarro@gmail.com>
2024-02-03 04:04:37 -0300

    Update CSS and JSX for social media buttons (generated by blackbox.ai )

--- src/components/molecules/SocialButtons/SocialButtons.jsx
+++ src/components/molecules/SocialButtons/SocialButtons.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import { faFacebookF, faTwitter, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import SocialButton from '../../atoms/SocialButton/SocialButton';
+import "./SocialButtons.css"
 
 const SocialButtons = (bgColor = 'transparent') => {
     return (
-        <div className="h-100 bg-transparent d-inline-flex box-rrss">
+        <div className="h-full inline-flex box-rrss">
             <SocialButton icon={faFacebookF} href="/" />
             <SocialButton icon={faTwitter} href="/" />
             <SocialButton icon={faLinkedinIn} href="/" />
