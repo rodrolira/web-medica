0baa0c81944829ad02ecb686ab9b1674aa7ff347
rodrolira <rlirapizarro@gmail.com>
2024-02-03 02:42:57 -0300

    Update dependencies and add a custom spinner component (generated by blackbox.ai )

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spinner } from 'flowbite-react';
-import './Spinner.css'
-
-const CustomSpinner = () => (
-  <Spinner show={true} bgColor="bg-white" position="fixed translate-middle" size="3" />
-);
-
-export default CustomSpinner;
