31e33822f12813b9a4c1ec136ccceca1b79fd5a5
rodrolira <rlirapizarro@gmail.com>
2024-02-03 04:05:49 -0300

    Update CSS for top bar and social buttons (generated by blackbox.ai )

--- src/components/atoms/SocialButton/SocialButton.jsx
+++ src/components/atoms/SocialButton/SocialButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./SocialButton.css"
 
 const SocialButton = ({ icon, href }) => {
     return (
