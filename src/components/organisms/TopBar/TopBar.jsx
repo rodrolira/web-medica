820fa0570f9150011dac19002f0b4b40781f09b5
rodrolira <rlirapizarro@gmail.com>
2024-02-03 04:03:49 -0300

    Refactor CSS for TopBar component (generated by blackbox.ai )

--- src/components/organisms/TopBar/TopBar.jsx
+++ src/components/organisms/TopBar/TopBar.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 import './TopBar.css';
 import SocialButtons from '../../molecules/SocialButtons/SocialButtons';
+import AddressInfo from '../../molecules/AddressInfo/AddressInfo';
 
 const TopBar = () => {
   return (
-    <div className="bg-light bg-blue p-0 wow fadeIn" data-wow-delay="0.1s">
-      <div className="hidden lg:flex h-tpbar container mx-auto">
+    <div className="bg-primary p-0 wow fadeIn container-fluid container-topbar flex w-full" data-wow-delay="0.1s">
+      <div className="row gx-0 w-full  h-topbar">
+        <div className="col-lg-7 top-bar border-box-1">
+          <AddressInfo />
+        </div>
+      </div>
+      <div className="hidden lg:flex h-topbar container mx-auto">
         <div className="top-bar border-box-1 lg:w-7/12">
           {/* ... (rest of the code for Topbar) */}
         </div>
-        <div className="h-topbar lg:w-5/12">
+        <div className="h-topbar w-full lg:w-5/12">
           {/* ... (rest of the code for Topbar) */}
           <SocialButtons />
         </div>
