23ad09489c70966a1d0a566382c29b832b5587ba
rodrolira <rlirapizarro@gmail.com>
2024-02-10 06:25:10 -0300

    Changed the background color of the back-to-top button on hover and added a rounded pill class. (generated by blackbox.ai )

---
+++ src/components/molecules/Title/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = ({ text }) => (
+    <p className="d-inline-block border rounded rounded-pill py-1 px-4">
+        {text}
+    </p>
+);
+
+export default Title;
