b5d528461cd401282617dc1ffcc98eef2c87eb60
rodrolira <rlirapizarro@gmail.com>
2024-02-09 21:41:52 -0300

    Create AddresInfo Component

---
+++ src/components/molecules/AddressInfo/AddressInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import "./AddressInfo.css"
+
+const AddressInfo = () => {
+    return (
+        <div className="inline-flex items-center box-address">
+            <big className="fa fa-map-marker-alt text-light icon-address" aria-hidden="true">
+                <FontAwesomeIcon icon={faMapMarkerAlt} />
+            </big>
+            <a className="fs-6 btn-address" href="contact.html">
+                <small className="top-bar address" id="direccion">Pucará 020, Machalí</small>
+            </a>
+        </div>
+    );
+};
+
+export default AddressInfo;
