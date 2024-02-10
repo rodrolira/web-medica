be182ce5b4daba9714bdf603f77eedc5bd8e335e
rodrolira <rlirapizarro@gmail.com>
2024-02-10 06:23:47 -0300

    Changed the border style of the Counter component from "border-start" to "border-l". (generated by blackbox.ai )

--- src/components/atoms/Counter/Counter.jsx
+++ src/components/atoms/Counter/Counter.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./Counter.css"
 
 const Counter = ({ number, text }) => {
     return (
         <div className="col-sm-4">
-            <div className="border-start border-light ps-4">
+            <div className="border-l border-light ps-4">
                 <h2 className="text-white mb-1">{number}</h2>
-                <p className="text-light mb-0">{text}</p>
+                <p className="text-white mb-0">{text}</p>
             </div>
         </div>
     );
