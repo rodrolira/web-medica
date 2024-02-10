a67f25488d872b2fbf4063de17437004b9d457aa
rodrolira <rlirapizarro@gmail.com>
2024-02-09 23:34:31 -0300

    Logo Component

---
+++ src/components/atoms/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../../public/images/logo.png"
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img loading="lazy" src="../../../images/logo.png" alt="logo centro nueva salud" />
+        </div>
+    );
+};
+
+export default Logo;
