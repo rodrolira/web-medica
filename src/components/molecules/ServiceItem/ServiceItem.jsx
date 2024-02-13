f9ab28519ee76ff69ae53358090f8cb90fb8eee1
rodrolira <rlirapizarro@gmail.com>
2024-02-13 03:35:36 -0300

    Update CSS for ServiceItem component and add more specialties. (generated by blackbox.ai )

--- src/components/molecules/ServiceItem/ServiceItem.jsx
+++ src/components/molecules/ServiceItem/ServiceItem.jsx
@@ -5,20 +5,22 @@ import Icon from '../../atoms/Icon/Icon';
 import { faPlus } from '@fortawesome/free-solid-svg-icons'; // Importa el ícono correctamente
 
 const ServiceItem = ({ icon, title, description, listItems, link }) => (
-    <div className="col-lg-3 col-md-6 wow fadeInUp column-services p-5" data-wow-delay="0.1s" style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeInUp' }}>
+    <div className="col-lg-4 col-md-6 wow fadeInUp column-services p-5" data-wow-delay="0.1s" style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeInUp' }}>
         <div className="service-item rounded h-100 p-12 box-especialidades">
-            <div className="inline-flex items-center justify-center bg-white rounded-circle mb-4" style={{ width: '65px', height: '65px' }}>
+            <div className="inline-flex items-center justify-center bg-white rounded-circle mb-4 mx-auto" style={{ width: '65px', height: '65px' }}>
                 <Icon icon={icon} color="primary" size="3xl" /> {/* Pasar el ícono como una propiedad */}
             </div>
-            <h4 className="mb-3 text-2xl">{title}</h4>
+            <h4 className="mb-3 text-2xl h-16 text-center">{title}</h4>
             <p className="mb-4 service-txt">{description}</p>
             <ol className='service-txt ms-8'>
-                {listItems.map((item, index) => (
+                {listItems?.map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ol>
-            <a className="btn btn-light dropdown-column" href={link}>
-                <Icon cla icon={faPlus} color="primary" size="3" /><span className='ms-3'>Ver Más</span></a> {/* Pasar el ícono como una propiedad */}
+            <div style={{ marginBlockStart: 'auto', display: 'flex' }}>
+                <a className="btn btn-light dropdown-column" href={link}>
+                    <Icon icon={faPlus} color="primary" size="3" /><span className='ms-3'>Ver Más</span></a>
+            </div>{/* Pasar el ícono como una propiedad */}
         </div>
     </div>
 );
--- src/components/organisms/Specialties/Specialties.jsx
+++ src/components/organisms/Specialties/Specialties.jsx
@@ -3,7 +3,7 @@ import Title from '../../molecules/Title/Title';
 import ServiceItem from '../../molecules/ServiceItem/ServiceItem';
 import SectionTitle from '../../atoms/SectionTitle/SectionTitle';
 import "./Specialties.css";
-import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
+import { faHeartbeat, faBaby, faBrain, faCrutch, faHeadSideVirus, faStethoscope } from '@fortawesome/free-solid-svg-icons';
 
 
 const Specialties = () => (
@@ -13,7 +13,7 @@ const Specialties = () => (
                 <SectionTitle title="Servicios" />
                 <Title text="Especialidades" />
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
                 {/* Add more ServiceItem components here for each specialty */}
                 <ServiceItem
                     icon={faHeartbeat}
@@ -23,39 +23,39 @@ const Specialties = () => (
                     link="especialidades/otorrino.html"
                 />
                 <ServiceItem
-                    icon="fa-heartbeat"
-                    title="Otorrinolaringología (Adulto e Infantil)"
-                    description="Enfocados en el tratamiento de patologías asociadas al oído, nariz y cavidades paranasales, cavidad oral, faringe y laringe."
-                    listItems={['Hipoacusia (disminución auditiva).', 'Rinitis.', 'Apnea del sueño.', 'Otitis.', 'Amigdalitis.', 'Fracturas nasales.', 'Mareos y vértigo.']}
-                    link="especialidades/otorrino.html"
+                    icon={faBaby}
+                    title="Pediatría"
+                    description="
+                Especializados tanto en pacientes recién nacidos como también
+                niños de 14 años, encargandose de multiples dolencias,
+                resfriados de distinto indoles. Como También nos preocupamos de
+                enseñar a los padres sobre como actuar ante estas, sobre todo en
+                recién nacidos."
+                    link="/"
                 />
                 <ServiceItem
-                    icon="fa-heartbeat"
-                    title="Otorrinolaringología (Adulto e Infantil)"
-                    description="Enfocados en el tratamiento de patologías asociadas al oído, nariz y cavidades paranasales, cavidad oral, faringe y laringe."
-                    listItems={['Hipoacusia (disminución auditiva).', 'Rinitis.', 'Apnea del sueño.', 'Otitis.', 'Amigdalitis.', 'Fracturas nasales.', 'Mareos y vértigo.']}
-                    link="especialidades/otorrino.html"
+                    icon={faBrain}
+                    title="Neurología Infantil"
+                    description="Especializado en tratar trastornos mentales las cuales pueden venir desde la conceción como el Autismo, Sindrome de Asperger. Como también trastornos desarrollados durante la niñez como es el caso del TDAH."
+                    link="/"
                 />
                 <ServiceItem
-                    icon="fa-heartbeat"
-                    title="Otorrinolaringología (Adulto e Infantil)"
-                    description="Enfocados en el tratamiento de patologías asociadas al oído, nariz y cavidades paranasales, cavidad oral, faringe y laringe."
-                    listItems={['Hipoacusia (disminución auditiva).', 'Rinitis.', 'Apnea del sueño.', 'Otitis.', 'Amigdalitis.', 'Fracturas nasales.', 'Mareos y vértigo.']}
-                    link="especialidades/otorrino.html"
+                    icon={faCrutch}
+                    title="Traumatología y Ortopedia de Adulto"
+                    description="Especialidad que se encarga en el estudio de afecciones de los musculos y del esqueleto como tambien lesiones, fracturas y hasta de enfermedades congenitas como la artrosis. Este campo tiene una variedad de sub-especialidades. Las cuales nosotros contamos con especialistas en Hombro/Codo, Rodilla, Cadera, y en su defecto Traumatologos Generales que ven toda parte del cuerpo."
+                    link="/"
                 />
                 <ServiceItem
-                    icon="fa-heartbeat"
-                    title="Otorrinolaringología (Adulto e Infantil)"
-                    description="Enfocados en el tratamiento de patologías asociadas al oído, nariz y cavidades paranasales, cavidad oral, faringe y laringe."
-                    listItems={['Hipoacusia (disminución auditiva).', 'Rinitis.', 'Apnea del sueño.', 'Otitis.', 'Amigdalitis.', 'Fracturas nasales.', 'Mareos y vértigo.']}
-                    link="especialidades/otorrino.html"
+                    icon={faHeadSideVirus}
+                    title="Psicología"
+                    description="Estamos especializados en el manejo y tratamiento, tanto médico como quirúrgico, de pacientes con enfermedades y alteraciones del oído, nariz, garganta y estructuras relacionadas de la cabeza y del cuello."
+                    link="/"
                 />
                 <ServiceItem
-                    icon="fa-heartbeat"
-                    title="Otorrinolaringología (Adulto e Infantil)"
-                    description="Enfocados en el tratamiento de patologías asociadas al oído, nariz y cavidades paranasales, cavidad oral, faringe y laringe."
-                    listItems={['Hipoacusia (disminución auditiva).', 'Rinitis.', 'Apnea del sueño.', 'Otitis.', 'Amigdalitis.', 'Fracturas nasales.', 'Mareos y vértigo.']}
-                    link="especialidades/otorrino.html"
+                    icon={faStethoscope}
+                    title="Kinesiología y Quiropraxia"
+                    description="Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet."
+                    link="/"
                 />
             </div>
         </div>
