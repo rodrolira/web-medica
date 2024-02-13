5d94f19f9512d52a2397c78bed7d51b534fc9ddd
rodrolira <rlirapizarro@gmail.com>
2024-02-13 02:23:12 -0300

    Changed the `Counter` component to have a `py-4` class instead of `ps-4`. (generated by blackbox.ai )

--- src/components/atoms/Counter/Counter.jsx
+++ src/components/atoms/Counter/Counter.jsx
@@ -4,7 +4,7 @@ import "./Counter.css"
 const Counter = ({ number, text }) => {
     return (
         <div className="col-sm-4">
-            <div className="border-l border-light ps-4">
+            <div className="border-l border-light py-4">
                 <h2 className="text-white mb-1">{number}</h2>
                 <p className="text-white mb-0">{text}</p>
             </div>
