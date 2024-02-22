159246773382dd1bf7c0f492f445864569e791f7
rodrolira <rlirapizarro@gmail.com>
2024-02-21 22:25:06 -0300

    Update CSS for ScheduleAppointment and Header components (generated by blackbox.ai )

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -8,15 +8,13 @@ import './Header.css';
 
 
 const Header = () => {
-
-
     return (
         <div className="container-fluid header bg-primary !p-0 mb-10 w-full">
             <div className="row g-0 items-center w-full [lg]:flex-row page-header !flex-nowrap text-center ">
-                {/* MENU 2 */}
+                {/* LeftSide */}
                 <PageHeader />
 
-                {/* MENU 1 */}
+                {/* RigthSide */}
                 <div className="col-lg-6 w-[50%]  wow fadeIn menu-1 p-[3rem]" data-wow-delay="0.1s" style={{ visibility: 'visible', animationDelay: '0.1s', animationName: 'fadeIn' }}>
                     <h1 className="display-4 text-white mb-5">
                         <b>Centro Médico y Diagnóstico Nueva Salud</b>
